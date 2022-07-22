6f1b844eb97994ff5c4b61b520e8fc3b3be06e8b
paweleck <czemierysp@op.pl>
2022-07-22 10:37:48 +0200

    proxy and mockoon

--- proxy.conf.js
+++ proxy.conf.js
@@ -10,7 +10,7 @@ const proxyConfig = [
   {
     context: '/api',
     pathRewrite: { '^/api': '' },
-    target: 'https://api.chucknorris.io',
+    target: 'http://localhost:3001',
     changeOrigin: true,
     secure: false,
   },
